2471b7de9bd53eff2c04fb06461f30bcfcf8184b
hirukaya88 <hirukaranumitha@gmail.com>
2025-01-04 08:01:28 +0530

    Update main.yml

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "6uZRDDoR#QfJbc3x0kztqR4fe-8WTjFYWQ_y_kH5nskoq2bs9I7A", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "zzJGjY4Q#YVAGQQY-9CM92NMLQ5scLsuWuzAx893Gz09Yu5qo0j8", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
